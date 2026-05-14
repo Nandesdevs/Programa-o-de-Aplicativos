1f1053135e227028d8c51c96fc4c60d884c494c8
Nandes_ <hernandessantos102@gmail.com>
2026-05-14 14:17:16 -0300

    API Lista de Tarefas
    API simples feita pelo meu professor utilizando Flask, irei estudar em cima dela.

--- Aulas ao vivo/Aula 04 ao vivo tercio/app.py
+++ Aulas ao vivo/Aula 04 ao vivo tercio/app.py
@@ -1,15 +1,32 @@
 # Importa o Flask
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+# Importar a classe tarefas para nosso app que é o programa principal
+from models import Tarefa
 
 # Essa linha diz = Flask apartir daqui começe a trabalhar
 app = Flask(__name__)
+
+# lista para simular um mini banco de dados
+tarefas = []
+contador_id = 1
+
 
 # Criação de uma rota
 # Decorador app route diz para o flask, quando alguem executar uma pagina inicial(representada por('/') ao lado de app route) execute a função logo abaixo
 @app.route('/')
 def index():
-    return '<h1>Olá, seja bem-vindo(a) ao meu primeiro site em Flask!</h1>''<br>''<p>Esse tal de Flask é bom mermo...</p>'
+    return render_template('index.html',lista_tarefas = tarefas)
 
+@app.route('/adicionar', methods = ['POST'])
+def adicionar():
+    global contador_id
+    descricao = request.form.get('descricao')
+    nova_tarefa = Tarefa(contador_id, descricao)
+    tarefas.append(nova_tarefa)
+    contador_id += 1
+    return redirect(url_for('index'))
 # Esse if name é o nosso "melhor amigo", se a gente precisar alterar o codigo ele reinicia o server sozinho, mostra os erros que tiver e atualiza automaticamente
+# Esse main é o corpo principal do programa
 if __name__ == '__main__':
     app.run(debug = True)
